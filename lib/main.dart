0252b907140ddbf465e38d8d6e101e9c4a72e627
Mohamad Rizal Andy Alkarim <rzalandy@gmail.com>
2021-07-14 09:14:25 +0700

    Flutter List Grid - Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:yard/screen/simple_list.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Belajar Flutter',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Listview & GridView'),
+      ),
+
+      body: Center(
+        child: Column(
+          children: [
+            Text('Listview & Gridview'),
+            Container(
+              padding: EdgeInsets.all(16),
+              child: MaterialButton(
+                child: Text('Simple List'),
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=> PageSimpleList()));
+                },
+                color: Colors.green,
+                textColor: Colors.white,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(16),
+              child: MaterialButton(
+                child: Text('Horizontal List'),
+                onPressed: (){
+
+                },
+                color: Colors.green,
+                textColor: Colors.white,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screen/simple_list.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+  class PageSimpleList extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Simple List'),
+          backgroundColor: Colors.green,
+        ),
+
+        body: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: ListTile(
+                leading: Icon(Icons.access_alarm),
+                title: Text('Alarm'),
+              ),
+            ),
+
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('Phone'),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Message'),
+            ),
+            ListTile(
+              leading: Icon(Icons.camera),
+              title: Text('Camera'),
+            ),
+          ],
+        )
+      );
+    }
+  }
